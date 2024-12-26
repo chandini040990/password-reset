11cf123049ac39cc193a9ad2e9356686cf093f4d
chandini <chandinisureshkumar@gmail.com>
2024-12-26 20:16:25 +0530

    commit

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -3,7 +3,7 @@ import axios from "axios";
 // set the base url for the api for the backend
 
 const api = axios.create({
-    baseURL: `https://chand2-password-reset.netlify.app/api`,
+    baseURL: `https://password-reset-qhp3.onrender.com/api`,
 })
 
 export default api;
