955340741d678481c0bfd47d11f7f863d522ce2a
chandini <chandinisureshkumar@gmail.com>
2024-12-27 15:04:42 +0530

    change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,18 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
 import Register from "./Register";
 import Login from "./Login";
-import './App.css';
 import ResetPassword from "./ResetPassword";
+import './App.css';
+
 
 function App() {
   return (
     <Router>
     <div className="App">
       <Routes>
-        <Route path="https://chand-password-reset.netlify.app/" element = {<Register />} />
-        <Route path="https://chand-password-reset.netlify.app/login" element = {<Login />} />
-        <Route path="https://chand-password-reset.netlify.app/resetpassword" element = {<ResetPassword />} />
+        <Route path="/" element = {<Register />} />
+        <Route path="/login" element = {<Login />} />
+        <Route path="/resetpassword" element = {<ResetPassword />} />
       </Routes>
     </div>
     </Router>
